1f2c6c01f008a8365a1cae5640115da3eac5ba8d
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-06-21 23:00:25 +0200

    Import SlideFromLeft in landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { Testimonials } from "@/components/Testimonials";
 import { FindUs } from "@/components/FindUs";
 import { services, trustPoints } from "@/lib/static-data";
 import { HeroAnimation } from "@/components/HeroAnimation";
+import { SlideFromLeft } from "@/components/SlideFromLeft";
 
 
 export default function Home() {
